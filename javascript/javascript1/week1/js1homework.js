893601c5980f4dd57903430c6d3c1db4ac76d0be
ReenuMary <reenumaryabraham@gmail.com>
2022-05-24 10:51:47 +0200

    review comments and constructor method for house price

--- javascript/javascript1/week1/js1homework.js
+++ javascript/javascript1/week1/js1homework.js
@@ -5,15 +5,15 @@ const userName = "Reenu";
 const currentAge = new Date().getFullYear() - yearOfBirth;
 const futureAge = futureYear - yearOfBirth;
 
-console.log(userName + " is " + currentAge + " years old now.");
-console.log("She will be " + futureAge + " years old in " + futureYear);
+console.log(`${userName} is ${currentAge} years old now.`);
+console.log(`She will be ${futureAge} years old in ${futureYear}`);
 
 //Excercise2
 //"1 dog year = 7 human years"
 
 const dogYearOfBirth = 2015;
 const dogYearFuture = 2025;
-let shouldShowResultInDogYears = true;
+const shouldShowResultInDogYears = true;
 const dogAgeInFuture = dogYearFuture - dogYearOfBirth;
 if (shouldShowResultInDogYears) {
   console.log(
@@ -26,65 +26,39 @@ if (shouldShowResultInDogYears) {
 }
 
 //Excercise 3
-//housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
-const houseDimensionPeter = [8, 10, 10];
-const houseVolumePeter =
-  houseDimensionPeter[0] * houseDimensionPeter[1] * houseDimensionPeter[2];
-const gardenSizePeter = 100;
-const calculatedHousePricePeter =
-  houseVolumePeter * 2.5 * 1000 + gardenSizePeter * 300;
-const actualPricePaidPeter = 2500000;
 
-console.log("Calculated volume for Peter's house " + houseVolumePeter);
-console.log("Calculated price for Peter's house " + calculatedHousePricePeter);
-console.log("Actual price paid for Peter's house " + actualPricePaidPeter);
-
-if (calculatedHousePricePeter > actualPricePaidPeter) {
-  console.log(
-    `Peter paid 
-      ${calculatedHousePricePeter - actualPricePaidPeter}
-       less than the calculated price. `
-  );
-} else if (calculatedHousePricePeter == actualPricePaidPeter) {
-  console.log(
-    `Peter paid ${actualPricePaidPeter}. Same as the calculated price`
-  );
-} else {
-  console.log(
-    `Peter paid   
-      ${actualPricePaidPeter - calculatedHousePricePeter} more than
-      the calculated price. `
-  );
+function HousePrice(houseDimension, gardenSize, actualPricePaid, userName) {
+  this.houseDimension = houseDimension;
+  this.gardenSize = gardenSize;
+  this.actualPricePaid = actualPricePaid;
+  this.houseVolume = houseDimension[0] * houseDimension[1] * houseDimension[2];
+  this.housePrice = this.houseVolume * 2.5 * 1000 + this.gardenSize * 300;
+  this.differenceInPrice = this.actualPricePaid - this.housePrice;
+  this.userName = userName;
+  this.analysePrice = function () {
+    if (this.differenceInPrice > 0) {
+      console.log(
+        `${this.userName} paid ${this.differenceInPrice} more than the calculated price which is ${this.housePrice}`
+      );
+    } else if (this.differenceInPrice === 0) {
+      console.log(
+        `${this.userName} paid the same price as the calculated price`
+      );
+    } else {
+      console.log(
+        `${this.userName} paid ${
+          this.differenceInPrice * -1
+        } less than the calculated price  which is ${this.housePrice}`
+      );
+    }
+  };
 }
-const houseDimensionJulia = [5, 11, 8];
-const houseVolumeJulia =
-  houseDimensionJulia[0] * houseDimensionJulia[1] * houseDimensionJulia[2];
-const gardenSizeJulia = 70;
-const calculatedHousePriceJulia =
-  houseVolumeJulia * 2.5 * 1000 + gardenSizeJulia * 300;
-const actualPricePaidJulia = 1000000;
 
-console.log("Calculated volume for Julia's house " + houseVolumeJulia);
-console.log("Calculated price for Julia's house " + calculatedHousePriceJulia);
-console.log("Actual price paid for Julia's house " + actualPricePaidJulia);
-
-if (calculatedHousePriceJulia > actualPricePaidJulia) {
-  console.log(
-    `Julia paid 
-      ${calculatedHousePriceJulia - actualPricePaidJulia}
-       less than the calculated price. `
-  );
-} else if (calculatedHousePriceJulia == actualPricePaidJulia) {
-  console.log(
-    `Julia paid ${actualPricePaidJulia}. Same as the calculated price`
-  );
-} else {
-  console.log(
-    `Julia paid   
-      ${actualPricePaidJulia - calculatedHousePriceJulia} more than
-      the calculated price. `
-  );
-}
+const housePricePeter = new HousePrice([8, 10, 10], 100, 2500000, "Peter");
+housePricePeter.analysePrice();
+const housePriceJulia = new HousePrice([5, 11, 8], 70, 1000000, "Julia");
+housePriceJulia.analysePrice();
+//console.log(housePricePeter);
 
 //Excercise 4
 const firstWords = [
