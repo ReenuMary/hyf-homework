36b6aa878c7c461fed49ddc420561847a12def9c
ReenuMary <reenumaryabraham@gmail.com>
2022-05-25 13:57:45 +0200

    methods redefined

--- javascript/javascript1/week1/js1homework2.js
+++ javascript/javascript1/week1/js1homework2.js
@@ -4,24 +4,33 @@ function HousePrice(houseDimension, gardenSize, actualPricePaid, userName) {
   this.houseDimension = houseDimension;
   this.gardenSize = gardenSize;
   this.actualPricePaid = actualPricePaid;
-  this.houseVolume = houseDimension[0] * houseDimension[1] * houseDimension[2];
-  this.housePrice = this.houseVolume * 2.5 * 1000 + this.gardenSize * 300;
-  this.differenceInPrice = this.actualPricePaid - this.housePrice;
   this.userName = userName;
+
+  this.houseVolume = function () {
+    return houseDimension[0] * houseDimension[1] * houseDimension[2];
+  };
+
+  this.housePrice = function () {
+    return this.houseVolume() * 2.5 * 1000 + this.gardenSize * 300;
+  };
+
   this.analysePrice = function () {
-    if (this.differenceInPrice > 0) {
+    const differenceInPrice = this.actualPricePaid - this.housePrice();
+    if (differenceInPrice > 0) {
       console.log(
-        `${this.userName} paid ${this.differenceInPrice} more than the calculated price which is ${this.housePrice}`
+        `${
+          this.userName
+        } paid ${differenceInPrice} more than the calculated price which is ${this.housePrice()}`
       );
-    } else if (this.differenceInPrice === 0) {
+    } else if (differenceInPrice === 0) {
       console.log(
         `${this.userName} paid the same price as the calculated price`
       );
     } else {
       console.log(
         `${this.userName} paid ${
-          this.differenceInPrice * -1
-        } less than the calculated price  which is ${this.housePrice}`
+          differenceInPrice * -1
+        } less than the calculated price  which is ${this.housePrice()}`
       );
     }
   };
@@ -31,4 +40,3 @@ const housePricePeter = new HousePrice([8, 10, 10], 100, 2500000, "Peter");
 housePricePeter.analysePrice();
 const housePriceJulia = new HousePrice([5, 11, 8], 70, 1000000, "Julia");
 housePriceJulia.analysePrice();
-//console.log(housePricePeter);
