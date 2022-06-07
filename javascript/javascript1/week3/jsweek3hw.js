391b2e0d0a7d4aa546ca69df41eeb137300a078f
ReenuMary <reenumaryabraham@gmail.com>
2022-06-07 12:31:56 +0200

    Ex2

--- javascript/javascript1/week3/jsweek3hw.js
+++ javascript/javascript1/week3/jsweek3hw.js
@@ -29,3 +29,35 @@ removeNameFromArray(nameToRemove);
 // Code done
 
 console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
+
+//Ex 2
+const travelInformation = {
+  speed: 50,
+  destinationDistance: 432,
+};
+
+function getTravelTime(travelInformation) {
+  if (
+    arguments.length < 1 ||
+    !travelInformation.hasOwnProperty("destinationDistance") ||
+    !travelInformation.hasOwnProperty("speed")
+  ) {
+    return "Invalid travel information";
+  }
+  if (
+    typeof travelInformation.destinationDistance != "number" ||
+    typeof travelInformation.speed != "number"
+  ) {
+    return "Destination distance and speed must be numbers";
+  }
+  const time = travelInformation.destinationDistance / travelInformation.speed;
+  const hours = Math.floor(time);
+  const minutes = Math.floor((time % 1) * 60);
+  return `${hours} hours and ${minutes} minutes`;
+}
+console.log(getTravelTime());
+console.log(getTravelTime(2));
+console.log(getTravelTime({ destinationDistance: 100, speed: "25 " }));
+
+const travelTime = getTravelTime(travelInformation);
+console.log(travelTime); // 8 hours and 38 minutes
