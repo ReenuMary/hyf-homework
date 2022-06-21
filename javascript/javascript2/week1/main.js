b0d3ee3991d18e42f11b72b554036c1fc297c042
ReenuMary <reenumaryabraham@gmail.com>
2022-06-21 09:30:36 +0200

    danish letter count

--- javascript/javascript2/week1/main.js
+++ javascript/javascript2/week1/main.js
@@ -16,3 +16,31 @@ function getShortestWord(danishWords) {
   });
   return shortestWord;
 }
+
+//Find and count the Danish letters
+function getWordAnddanishLetterCount(danishString) {
+  if (arguments.length < 1 || typeof danishString != "string") {
+    return "Invalid argument";
+  }
+
+  const object = { total: 0 };
+  const aeCount = (danishString.match(/æ/gi) || []).length;
+  const åCount = (danishString.match(/å/gi) || []).length;
+  const øCount = (danishString.match(/ø/gi) || []).length;
+
+  if (aeCount > 0) {
+    object["æ"] = aeCount;
+  }
+  if (åCount > 0) {
+    object["å"] = åCount;
+  }
+  if (øCount > 0) {
+    object["ø"] = øCount;
+  }
+  object.total = aeCount + åCount + øCount;
+  return object;
+}
+const danishString = "Jeg har en blå bil";
+console.log(getWordAnddanishLetterCount(danishString));
+const danishString2 = "Blå grød med røde bær";
+console.log(getWordAnddanishLetterCount(danishString2));
