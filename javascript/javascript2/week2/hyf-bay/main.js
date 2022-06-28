7896747b8a508f075e2622e60b3cfb76f8240ffc
ReenuMary <reenumaryabraham@gmail.com>
2022-06-28 14:54:02 +0200

    price filteration

--- javascript/javascript2/week2/hyf-bay/main.js
+++ javascript/javascript2/week2/hyf-bay/main.js
@@ -1,4 +1,58 @@
-console.log("Script loaded");
-
 const products = getAvailableProducts();
-console.log(products);
+function createLiElementWithText(text) {
+  const liElement = document.createElement("li");
+  liElement.innerHTML = text;
+  return liElement;
+}
+
+function renderProducts(products) {
+  const ulElement = document.getElementById("products");
+  ulElement.innerHTML = "";
+  for (const product of products) {
+    ulElement.appendChild(createLiElementWithText(product.name));
+    ulElement.appendChild(createLiElementWithText(`Price : ${product.price}`));
+    ulElement.appendChild(createLiElementWithText(`Rating: ${product.rating}`));
+  }
+}
+
+function applyFilterButtonClick() {
+  const maxPrice = parseFloat(document.getElementById("max-price").value);
+  const minPrice = parseFloat(document.getElementById("min-price").value);
+
+  const filter = {};
+  if (!isNaN(maxPrice)) filter.maxPrice = maxPrice;
+  if (!isNaN(minPrice)) filter.minPrice = minPrice;
+
+  const copyProducts = [...products];
+
+  const filteredProducts = filterProducts(copyProducts, filter);
+  renderProducts(filteredProducts);
+}
+
+function clearFilterButtonClick() {
+  document.getElementById("max-price").value = "";
+  document.getElementById("min-price").value = "";
+  renderProducts(products);
+}
+
+function filterProducts(products, filter) {
+  for (let key in filter) {
+    switch (key) {
+      case "maxPrice": {
+        products = products.filter(
+          (oneProduct) => oneProduct.price <= filter.maxPrice
+        );
+        break;
+      }
+      case "minPrice": {
+        products = products.filter(
+          (oneProduct) => oneProduct.price >= filter.minPrice
+        );
+        break;
+      }
+    }
+  }
+  return products;
+}
+
+renderProducts(products);
