c116b07d0fa9a9b07a94d0026059a38b05297f50
ReenuMary <reenumaryabraham@gmail.com>
2022-07-04 14:16:13 +0200

    warm up

--- javascript/javascript2/week3/index.js
+++ javascript/javascript2/week3/index.js
@@ -25,3 +25,67 @@ planetLogFunction(earthLogger);
 planetLogFunction(saturnLogger);
 
 //LOG LOCATION
+function getAndLogLocation() {
+  //navigator.geolocation
+  const statusText = document.querySelector("#status");
+  if (!navigator.geolocation) {
+    statusText.innerHTML = "Location not supported by browser";
+  } else {
+    statusText.innerHTML = "Loading location...";
+
+    function locationSuccess(position) {
+      document.getElementById(
+        "lbl-latitude"
+      ).innerHTML = `Latitude - ${position.coords.latitude}`;
+      document.getElementById(
+        "lbl-longitude"
+      ).innerHTML = `Longitude - ${position.coords.longitude}`;
+      statusText.innerHTML = "";
+
+      window.initMap = initMap();
+    }
+
+    function locationError(errorMessage) {
+      statusText.innerHTML = errorMessage;
+    }
+
+    const location = navigator.geolocation.getCurrentPosition(
+      locationSuccess,
+      locationError
+    );
+  }
+}
+
+const btnLocation = document.getElementById("btn-log-location");
+btnLocation.addEventListener("click", getAndLogLocation);
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+function runAfterDelay(delay, callback) {
+  setTimeout(callback, delay * 1000);
+}
+
+runAfterDelay(2, () => console.log("Running callback function after delay"));
+
+document.addEventListener("mouseup", logDoubleClick);
+let isfirstClick = false;
+function logDoubleClick() {
+  if (isfirstClick) console.log("double click");
+  isfirstClick = true;
+  setTimeout(() => (isfirstClick = false), 500);
+}
+
+const logFunnyJoke = () => console.log("Funny joke");
+const logBadJoke = () => console.log("Bad joke");
+
+const jokeCreator = (shouldTellFunnyJoke) =>
+  shouldTellFunnyJoke ? logFunnyJoke() : logBadJoke();
+
+jokeCreator(false);
