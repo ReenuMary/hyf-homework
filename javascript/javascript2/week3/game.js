de80a1ec712ed8c41ba29f90ebd0e0e71abc7ed2
ReenuMary <reenumaryabraham@gmail.com>
2022-07-05 23:41:35 +0200

    game with count down

--- javascript/javascript2/week3/game.js
+++ javascript/javascript2/week3/game.js
@@ -1,18 +1,43 @@
-let isGameStarted, lCounter, sCounter;
+let isGameStarted, lCounter, sCounter, countDown, intervalId;
 window.onload = function () {
   if (localStorage.getItem("hasCodeRunBefore") === null) {
     localStorage.setItem("hasCodeRunBefore", true);
     isGameStarted = false;
     lCounter = 0;
     sCounter = 0;
+    countDown = 0;
   }
 };
 
+function renderConfetti(elementId) {
+  var confettiElement = document.getElementById(elementId);
+  var confettiSettings = { target: confettiElement };
+  var confetti = new ConfettiGenerator(confettiSettings);
+  confetti.render();
+}
+
 function endOfGame() {
-  // sCounter ===lCounter ?
-  sCounter > lCounter
-    ? (document.getElementById("s-counter").innerHTML = "You Won !!!")
-    : (document.getElementById("l-counter").innerHTML = "You Won !!!");
+  isGameStarted = false;
+  clearInterval(intervalId);
+  if (sCounter === 0 && lCounter === 0) {
+    document.getElementById("s-message").innerHTML =
+      "Click start and press 's' to play";
+    document.getElementById("l-message").innerHTML =
+      "Click start and press 'l' to play";
+  } else {
+    if (sCounter === lCounter) {
+      document.getElementById("s-message").innerHTML = "Equal score";
+      document.getElementById("l-message").innerHTML = "Equal score";
+    } else {
+      if (sCounter > lCounter) {
+        document.getElementById("s-message").innerHTML = "You Won !!!";
+        // renderConfetti("s-canvas");
+      } else {
+        document.getElementById("l-message").innerHTML = "You Won !!!";
+        // renderConfetti("l-canvas");
+      }
+    }
+  }
 }
 
 function keyPressed(e) {
@@ -25,6 +50,13 @@ function keyPressed(e) {
       document.getElementById("l-counter").innerHTML = lCounter;
     }
   }
+}
+
+function timeLeftCounter() {
+  --countDown;
+  document.getElementById(
+    "count-down"
+  ).innerHTML = `Time left ${countDown} seconds.`;
 }
 
 function btnStartGameClick() {
@@ -40,10 +72,18 @@ function btnStartGameClick() {
     isGameStarted = true;
     sCounter = 0;
     lCounter = 0;
+    countDown = gameDurationSeconds - 1;
+
     document.getElementById("s-counter").innerHTML = sCounter;
     document.getElementById("l-counter").innerHTML = lCounter;
+    document.getElementById("s-message").innerHTML = "";
+    document.getElementById("l-message").innerHTML = "";
+    document.getElementById(
+      "count-down"
+    ).innerHTML = `Time left ${gameDurationSeconds} seconds.`;
 
     setTimeout(endOfGame, gameDurationSeconds * 1000);
+    intervalId = setInterval(timeLeftCounter, 1000);
   }
 }
 
