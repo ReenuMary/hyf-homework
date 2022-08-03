f85d7a1db947154584c7ef91bf6f8e5721141f11
ReenuMary <reenumaryabraham@gmail.com>
2022-08-03 12:10:33 +0200

    unsaved changes

--- javascript/javascript3/week1/giphySearch.js
+++ javascript/javascript3/week1/giphySearch.js
@@ -1,30 +1,34 @@
 function btnGifClick() {
   const searchKeyWord = document.getElementById("search-text").value;
   let noOfGifs = document.getElementById("gif-count").value;
-  if (
-    noOfGifs === "" ||
-    noOfGifs === null ||
-    noOfGifs === undefined ||
-    isNaN(noOfGifs)
-  )
-    noOfGifs = 5;
-  const url = new URL("https://api.giphy.com/v1/gifs/search");
-  const params = {
-    q: searchKeyWord,
-    limit: noOfGifs,
-    api_key: "pLg6OqlnCUK2JBmKNqWDJfHaFViJFvQ8",
-  };
 
-  Object.keys(params).forEach((key) => {
-    url.searchParams.append(key, params[key]);
-  });
+  if (searchKeyWord !== "") {
+    if (
+      noOfGifs === "" ||
+      noOfGifs === null ||
+      noOfGifs === undefined ||
+      isNaN(noOfGifs)
+    )
+      noOfGifs = 5;
+    const url = new URL("https://api.giphy.com/v1/gifs/search");
+    const params = {
+      q: searchKeyWord,
+      limit: noOfGifs,
+      api_key: "pLg6OqlnCUK2JBmKNqWDJfHaFViJFvQ8",
+    };
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((responseJson) => {
-      console.log(responseJson);
-      displaySearchResult(responseJson);
+    Object.keys(params).forEach((key) => {
+      url.searchParams.append(key, params[key]);
     });
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson);
+        displaySearchResult(responseJson);
+      });
+  } else {
+  }
 }
 
 function displaySearchResult(gifJson) {
