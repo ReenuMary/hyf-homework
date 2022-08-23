82e3a8f4d475e6544631c041597b5d92247487ba
ReenuMary <reenumaryabraham@gmail.com>
2022-08-23 14:21:04 +0200

    part3

--- databases/week2/HomeWork.sql
+++ databases/week2/HomeWork.sql
@@ -8,3 +8,29 @@ SET status_id=(SELECT id FROM status WHERE name='In progress' )
 WHERE id=36;
 UPDATE task SET status_id = (SELECT id FROM status WHERE name='Done') WHERE id=37;
 DELETE FROM task WHERE id=38;
+
+#PART3
+#Get all the tasks assigned to users whose email ends in @spotify.com
+SELECT user_task.user_id ,user.name , user_task.task_id,task.title ,user.email
+FROM user_task 
+JOIN user ON user_task.user_id = user.id
+JOIN task ON user_task.task_id =task.id
+WHERE user.email LIKE '%@spotify.com';
+
+#Get all the tasks for 'Donald Duck' with status 'Not started'
+SELECT user_task.task_id ,task.title FROM user_task 
+JOIN user ON user.id=user_task.user_id
+JOIN task ON task.id= user_task.task_id
+JOIN status on status.id= task.status_id
+WHERE user.name ="Donald Duck"
+AND status.name ="Not started";
+
+#Get all the tasks for 'Maryrose Meadows' that were created in september (hint: month(created)=month_number)
+SELECT user_task.task_id, task.title FROM user_task 
+JOIN task ON user_task.task_id= task.id
+JOIN user ON user.id= user_task.user_id
+WHERE user.name="Maryrose Meadows"
+AND month(created)=9 ;
+
+#Find how many tasks where created in each month
+SELECT  MONTHNAME(created) as Month, count(*) AS 'Task count' FROM task GROUP BY Month;
